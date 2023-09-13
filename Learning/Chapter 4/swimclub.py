69a9d79a70951584f3ca4723210a4585e31203ab
Bizzy Lachat <elachat@revlocal.com>
2023-09-13 10:29:01 -0400

    Chapter 4: List of Files: Functions, Modules & Files

---
+++ Learning/Chapter 4/swimclub.py
@@ -0,0 +1,34 @@
+import statistics
+
+FOLDER = '../Chapter 3/swimdata/'
+
+def read_swim_data(filename):
+    swimmer, age, distance, stroke = filename.removesuffix(".txt").split("-")
+
+    with open(FOLDER + filename) as file:
+        lines = file.readlines()
+
+    converts = []
+    times = lines[0].strip().split(",")
+
+    for t in times:
+
+        if ":" in t:
+            mins, rest = t.split(":")
+            secs, hundredths = rest.split(".")
+        else:
+            mins = 0
+            secs, hundredths = t.split(".")
+        
+        converts.append((int(mins) * 60 * 100) + (int(secs) * 100) + int(hundredths))
+
+    avg = statistics.mean(converts)
+
+    mins_secs, hundredths = str(round(avg / 100, 2)).split(".")
+    mins_secs = int(mins_secs)
+    mins = mins_secs // 60
+    secs = mins_secs - (mins * 60)
+
+    avg = str(mins) + ":" + str(secs) + "." + hundredths
+
+    return swimmer, age, distance, stroke, times, avg
