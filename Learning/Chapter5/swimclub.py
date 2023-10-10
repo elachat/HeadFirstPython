f039de3634a7768219f6342946af842e2510daff
Bizzy Lachat <elachat@revlocal.com>
2023-10-10 15:22:24 -0400

    Chapter 5: Formatted String Literals Make Charts from Data

--- Learning/Chapter5/swimclub.py
+++ Learning/Chapter5/swimclub.py
@@ -1,6 +1,8 @@
 import statistics
+import hfpy_utils
 
 FOLDER = '../Chapter3/swimdata/'
+CHARTS = "charts/"
 
 def read_swim_data(filename):
     swimmer, age, distance, stroke = filename.removesuffix(".txt").split("-")
@@ -24,11 +26,56 @@ def read_swim_data(filename):
 
     avg = statistics.mean(converts)
 
-    mins_secs, hundredths = str(round(avg / 100, 2)).split(".")
+    mins_secs, hundredths = f"{(avg / 100):.2f}".split(".")
     mins_secs = int(mins_secs)
     mins = mins_secs // 60
     secs = mins_secs - (mins * 60)
 
-    avg = f"{mins}:{secs}.{hundredths}"
+    avg = f"{mins}:{secs:0>2}.{hundredths}"
 
-    return swimmer, age, distance, stroke, times, avg
+    return swimmer, age, distance, stroke, times, avg, converts
+
+def produce_bar_chart(fn):
+    """
+        Given the name of a swimmer's file, produce an HTML/SVG-based bar chart.
+        Save the chart to the CHARTS folder. Return the path to the bar chart files.
+    """
+
+    swimmer, age, distance, stroke, times, avg, converts = read_swim_data(fn)
+
+    from_max = max(converts)
+    svgs = ""
+    times.reverse()
+    converts.reverse()
+    title = f"{swimmer} (Under {age}) {distance} {stroke}"
+    header = f"""<!DOCTYPE html>
+    <html>
+        <head>
+            <title>{title}</title>
+        </head>
+        <body>
+            <h3>{title}</h3>
+    """
+    body = ""
+
+    for n, t in enumerate(times):
+        bar_width = hfpy_utils.convert2range(converts[n], 0, from_max, 0, 400)
+        body = body + f"""
+                    <svg height="30" width="400">
+                        <rect height="30" width="{bar_width}" style="fill:rgb(0,0,255);" />
+                    </svg>{t}<br />
+                        """
+        
+    footer = f"""
+            <p>Average time: {avg}</p>
+        </body>
+    </html>
+    """
+
+    page = header + body + footer
+    save_to = f"charts/{fn.removesuffix('.txt')}.html"
+
+    with open(save_to, "w") as sf:
+        print(page, file=sf)
+    
+    return save_to
